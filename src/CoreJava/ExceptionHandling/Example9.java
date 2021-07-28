f9ea627985cbac4370b63df6fa0833ad1fdbbc18
Sai Krishna <kvskrishna712@gmail.com>
2021-07-28 08:57:12 +0530

    Adding Exception Handling Example9

---
+++ src/CoreJava/ExceptionHandling/Example9.java
@@ -0,0 +1,17 @@
+package CoreJava.ExceptionHandling;
+
+/*
+    After throw statement we are not allowed to write any statement directly otherwise
+    we get compile time error saying "unreachable statement".
+ */
+public class Example9 {
+    public static void main(String[] args){
+        throw new ArithmeticException("/ by zero");
+        System.out.println("UnReachable Statement");
+    }
+}
+/*
+    Output:
+    =======
+    java: unreachable statement
+ */
