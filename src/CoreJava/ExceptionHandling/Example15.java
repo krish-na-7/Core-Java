9701e0976e16ac065e25dad61d4d1b86489891c5
Sai Krishna <kvskrishna712@gmail.com>
2021-07-29 10:38:09 +0530

    Adding Exception Handling Example15

---
+++ src/CoreJava/ExceptionHandling/Example15.java
@@ -0,0 +1,18 @@
+package CoreJava.ExceptionHandling;
+
+/*
+      Case 1:
+      =======
+      We can use throws keyword only for constructors and methods but not for classes.
+ */
+public class Example15
+{
+        Example15() throws Exception
+        {
+
+        }
+        public void add() throws Exception{
+
+        }
+
+}
