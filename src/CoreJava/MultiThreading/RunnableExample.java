4079d196b8db3b3fcc98db8b2a31c86f4ee79b03
Sai Krishna <kvskrishna712@gmail.com>
2021-09-11 16:07:18 +0530

    Adding Multi Threading Runnable Example

---
+++ src/CoreJava/MultiThreading/RunnableExample.java
@@ -0,0 +1,50 @@
+package CoreJava.MultiThreading;
+
+public class RunnableExample
+{
+    public static void main(String[] args)
+    {
+          MyRunnable myRunnable = new MyRunnable();
+          Thread thread = new Thread(myRunnable);
+          thread.start();
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("In Main Thread !!!");
+        }
+    }
+}
+class MyRunnable implements Runnable
+{
+    public void run()
+    {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("In Child Thread !!!");
+        }
+    }
+}
+/*
+Output:
+=======
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Child Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+In Main Thread !!!
+
+ */
