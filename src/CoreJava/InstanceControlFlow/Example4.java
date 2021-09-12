809d32ae9e075e75d2e7f7e83b5a5a2256730f93
Sai Krishna <kvskrishna712@gmail.com>
2021-09-12 16:16:45 +0530

    Adding Instance Control Flow Example4

---
+++ src/CoreJava/InstanceControlFlow/Example4.java
@@ -0,0 +1,46 @@
+package CoreJava.InstanceControlFlow;
+
+public class Example4 {
+
+    {
+        System.out.println("In 1st Instance block");
+    }
+
+    static
+    {
+        System.out.println("In 1st Static block");
+    }
+    Example4()
+    {
+        System.out.println("In Constructor");
+    }
+
+    public static void main(String[] args)
+    {
+        Example4 c1 = new Example4();
+        System.out.println("main");
+        new Example4();
+
+    }
+    {
+        System.out.println("In 2nd Instance block");
+    }
+    static
+    {
+        System.out.println("In 2nd Static block");
+    }
+}
+/*
+Output:
+=======
+In 1st Static block
+In 2nd Static block
+In 1st Instance block
+In 2nd Instance block
+In Constructor
+main
+In 1st Instance block
+In 2nd Instance block
+In Constructor
+
+ */
